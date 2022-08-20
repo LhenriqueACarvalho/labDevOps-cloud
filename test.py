9b23e7541747f3ad7ad052e2f36371377b2ae399
LhenriqueACarvalho <lhenriqueacarvalho@gmail.com>
2022-08-20 10:56:44 -0300

    csrff

--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "LH Shop - Camisas de Times. Segue no Instagram @lh._shop")
+        self.assertEqual(self.result.data.decode('utf-8'), "Laboratório Pipeline DevOps")

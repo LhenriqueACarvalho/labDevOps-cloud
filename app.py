cacb9f8fc25156b5d3619b93bd81ec1610604139
LhenriqueACarvalho <lhenriqueacarvalho@gmail.com>
2022-08-20 10:54:51 -0300

    csrf protect

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)                                                                                                                           
+
 @app.route("/")
 def pagina_inicial():
-    return "LH Shop - Camisas de Times. Segue no Instagram @lh._shop"
+    return "Laboratório Pipeline DevOps"
 
 if __name__ == '__main__':
     app.run()
